11eb2a4f51ac377395513dae1f70f0c17753000e
Francesco Anselmo <francesco.anselmo@gmail.com>
2023-05-02 23:26:18 +0100

    remove libsmbclient-dev

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt install ca-certificates git-core binutils libasound2-dev libva1 libpcre3-dev libidn11-dev libboost-dev libfreetype6-dev libdbus-1-dev libssl1.0-dev libssh-dev 
-libsmbclient-dev gcc g++ sed pkg-config
+sudo apt install ca-certificates git-core binutils libasound2-dev libva1 libpcre3-dev libidn11-dev libboost-dev libfreetype6-dev libdbus-1-dev libssl1.0-dev libssh-dev gcc g++ sed pkg-config
 
 
